99cfacc6d64ca414e8b7c31a16b6650e7b9e9eef
MIDST lab <89193539+midstlab@users.noreply.github.com>
2023-07-17 15:58:21 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "GLASS",
-    version = "0.0.1",
-    description = "Grouping Ligand Alignment Sites Selectively",
-    executables = [Executable("main.py", base="Console")], # Use the name of your main python file here
-)
